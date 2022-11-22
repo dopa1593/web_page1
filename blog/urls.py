51efd3bf9d4bbd8384fd5fb724f89d7b83753848
dopa1593 <wkdqkfwkd123456@naver.com>
2022-11-22 11:41:58 +0900

    포스트 목록 페이지에 부트스트랩 적용하기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     #이부분을 채울 겁니다!
     path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
     # path('', views.index)
-    path('<int:pk>/', views.single_post_page),
+    # path('<int:pk>/', views.single_post_page),
 
 ]
