e7354c2b2172163df14e9129cf0d0c5883b21147
dopa1593 <wkdqkfwkd123456@naver.com>
2022-12-05 12:26:24 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 #blog/views.py
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
 # Create your views here.
 
@@ -23,6 +24,8 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
+
 
 def category_page(request, slug):
     if slug == 'no_category':
@@ -57,6 +60,17 @@ def tag_page(request, slug):
             'no_category_post_count' : Post.objects.filter(category=None).count(),
         }
     )
+
+class PostCreate(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
