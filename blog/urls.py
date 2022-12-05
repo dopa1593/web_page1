103c573859179d52a22a10fcd429741d25ea80f4
dopa1593 <wkdqkfwkd123456@naver.com>
2022-12-05 10:43:03 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #이부분을 채울 겁니다!
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
