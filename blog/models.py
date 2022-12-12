a7264a840364d0b5dce4c5f27fe65d2d1d086ea5
dopa1593 <wkdqkfwkd123456@naver.com>
2022-12-12 10:21:52 +0900

    댓글 수정 기능 추가하기 v1

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,19 @@
-#blog/models.py
 from django.db import models
 from django.contrib.auth.models import User
 from markdownx.models import MarkdownxField
-from markdownx.utils import markdown
+from markdown import markdown
+
 import os
 
-# Create your models here
-
 class Tag(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
-    
-    def get_absolute_url(self):
-        return f'/blog/tag/{self.slug}/'
 
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -25,26 +22,27 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    class Meta:
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}'
+
+    class Meta :
         verbose_name_plural = 'Categories'
 
-    def get_absolute_url(self):
-        return f'/blog/category/{self.slug}/'
-
-class Post(models.Model):
+# Create your models here.
+class Post(models.Model) :
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
     content = MarkdownxField()
 
-    head_image = models.ImageField(upload_to='blog/image/%Y/%m/%d/', blank=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     tags = models.ManyToManyField(Tag, blank=True)
 
@@ -62,3 +60,35 @@ class Post(models.Model):
 
     def get_content_markdown(self):
         return markdown(self.content)
+
+# class Comment(models.Model):
+#     post        = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     author      = models.ForeignKey(User, on_delete=models.CASCADE)
+#     content     = models.TextField()
+#     created_at  = models.DateTimeField(auto_now_add=True)
+#     modified_at = models.DateTimeField(auto_now=True)
+#
+#     def __str__(self):
+#         return f'{self.author}::{self.content}'
+#
+#     def get_absolute_url(self):
+#         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://api.adorable.io/avatars/60/{ self.author.username }.png'
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,15 @@
-#blog/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.text import slugify
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.utils.text import slugify
-from .models import Post, Category, Tag
+from django.shortcuts import get_object_or_404
+from . models import Post, Category, Tag, Comment
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
-# Create your views here.
 
-class PostList(ListView):
+# Create your views here.
+class PostList(ListView) :
     model = Post
-    ordering = '-pk'
-    # template_name = 'blog/index.html'
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -18,51 +17,18 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
-class PostDetail(DetailView):
+# Post 상세 보기
+class PostDetail(DetailView) :
     model = Post
 
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
-
-
-def category_page(request, slug):
-    if slug == 'no_category':
-        category = '미분류'
-        post_list = Post.objects.filter(category=None)
-    else:
-        category = Category.objects.get(slug=slug)
-        post_list = Post.objects.filter(category=category)
-
-    return render(
-        request,
-        'blog/post_list.html',
-        {
-            'post_list' : post_list,
-            'categories' : Category.objects.all(),
-            'no_category_post_count' : Post.objects.filter(category=None).count(),
-            'category' : category,
-        }
-    )
-
-def tag_page(request, slug):
-    tag = Tag.objects.get(slug=slug)
-    post_list = tag.post_set.all()
-
-    return render(
-        request,
-        'blog/post_list.html',
-        {
-            'post_list' : post_list,
-            'tag' : tag,
-            'categories' : Category.objects.all(),
-            'no_category_post_count' : Post.objects.filter(category=None).count(),
-        }
-    )
-
+# CreateView
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
@@ -90,13 +56,16 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
                         tag.slug = slugify(t, allow_unicode=True)
                         tag.save()
                     self.object.tags.add(tag)
+
             return response
+            # return super(PostCreate, self).form_valid(form)
         else:
             return redirect('/blog/')
 
+
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
     template_name = 'blog/post_update_form.html'
 
@@ -107,6 +76,7 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
             for t in self.object.tags.all():
                 tags_str_list.append(t.name)
             context['tags_str_default'] = '; '.join(tags_str_list)
+
         return context
 
     def dispatch(self, request, *args, **kwargs):
@@ -122,6 +92,7 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         tags_str = self.request.POST.get('tags_str')
         if tags_str:
             tags_str = tags_str.strip()
+
             tags_str = tags_str.replace(',', ';')
             tags_list = tags_str.split(';')
 
@@ -132,26 +103,66 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
                     tag.slug = slugify(t, allow_unicode=True)
                     tag.save()
                 self.object.tags.add(tag)
+
         return response
 
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
+def category_page(request, slug) :
+    if slug == 'no_category' :
+        category = '미분류'
+        post_lsit = Post.objects.filter(category=None)
+    else :
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
 
-#     return render(
-#         request,
-#         'blog/index.html',
-#         {
-#             'posts' : posts,
-#         }
-#     )
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': Post.objects.filter(category=category),
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
 
-# def single_post_page(request, pk):
-#     post = Post.objects.get(pk = pk)
-#
-#     return render(
-#         request,
-#         'blog/single_post_page.html',
-#         {
-#             'post' : post,
-#         }
-#     )
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+    )
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
