5d01719ddd9747e0af734597985eb51926da98f9
dopa1593 <wkdqkfwkd123456@naver.com>
2022-12-14 14:21:52 +0900

    도커에 PostgreSQL 설정 추가하기

--- web_page11/settings.py
+++ web_page11/settings.py
@@ -16,9 +16,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-1d%u748eyo$s4ec1+m3gr=o@3a%r9i-5=0uf56(1xr$=)&(*b6"
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1d%u748eyo$s4ec1+m3gr=o@3a%r9i-5=0uf56(1xr$=)&(*b6')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = int(os.environ.get('DEBUG', 1))
+
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -71,9 +76,13 @@ WSGI_APPLICATION = "web_page11.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get("SQL_USER", 'user'),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", 'password'),
+        'HOST': os.environ.get("SQL_HOST", 'localhost'),
+        'PORT': os.environ.get("SQL_PORT", '5432'),
     }
 }
 # Password validation
